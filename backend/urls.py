7789a1bc92ce5d0a8b383db427f27fbfaba8125d
SANTHOSH KUMAR - SK <98202037+SANTHOSHKUMAR392002@users.noreply.github.com>
2025-03-26 10:08:00 +0530

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import admin_login, admin_logout, dashboard, event_create, add_event, event_list, event_update, event_delete, member_list, event_members_page
 
 urlpatterns = [
-    path("admin-login/", admin_login, name="admin-login"),
+    path("/", admin_login, name="admin-login"),
     path("admin-logout/", admin_logout, name="admin-logout"),  
     path("dashboard/", dashboard, name="dashboard"),
     path('event-create/', event_create, name='event-create'),
